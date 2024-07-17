3a42d8639268e2e2807480dee52407061efcce2b
SUMAN JANA <sumanjanaled@gmail.com>
2024-07-17 09:50:29 +0530

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/system";
+import { GoogleTagManager } from '@next/third-parties/google'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+       <GoogleTagManager gtmId="G-LNMKM548TQ" />
       <body className={inter.className}>
         <NextUIProvider>
         {children}
