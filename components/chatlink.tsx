77100c029efd74843afd994a2dbef154556249a5
SUMAN JANA <sumanjanaled@gmail.com>
2024-07-17 09:38:39 +0530

    try new

--- components/chatlink.tsx
+++ components/chatlink.tsx
@@ -1,10 +1,8 @@
 "use client";
 import React from "react";
-import  BackgroundGradient  from "./ui/background-gradient";
-import { IconAppWindow } from "@tabler/icons-react";
-import Image from "next/image";
+
 import ClipboardCopy from "./clipboard";
-export  function Chatlink({a}) {
+export  function Chatlink() {
   return (
     <div>
   
